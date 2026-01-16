17d92ab2dcbfe56c75776422ef25451e902d0966
Dev Rudra <rudradev252@gmail.com>
2026-01-16 12:19:07 +0000

    fix: updated ecosystem.staging.cjs

--- ecosystem.staging.cjs
+++ ecosystem.staging.cjs
@@ -2,12 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'api-staging',
-      script: 'npm',
-      args: 'run start:prod',
+      script: 'dist/main.js',
       cwd: '/var/www/development-pms/pms-backend',
       exec_mode: 'fork',
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'staging',
         APP_ENV: 'staging',
         PORT: 4001,
       },

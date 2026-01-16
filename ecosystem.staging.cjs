b6f935a22d1f14565bc01fe473c025afa56740ad
Dev Rudra <75237456+devrudraa@users.noreply.github.com>
2026-01-16 17:09:45 +0530

    fix: updated staging pm2 config

--- ecosystem.staging.cjs
+++ ecosystem.staging.cjs
@@ -2,12 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'api-staging',
-      script: 'dist/main.js',
+      script: 'npm',
+      args: 'run start:prod',
       cwd: '/var/www/development-pms/pms-backend',
-      instances: 1,
       exec_mode: 'fork',
       env: {
-        NODE_ENV: 'staging',
+        NODE_ENV: 'production',
+        APP_ENV: 'staging',
         PORT: 4001,
       },
     },
